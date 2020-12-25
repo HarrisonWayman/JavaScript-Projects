482da83071f0e16e94dca58c5a4ddb71964a4a79
Harrison <HarryEricWay@outlook.com>
2020-12-24 20:16:18 -0700

    JavaScript Project 1

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+window.alert("Hello there, general Kenobi!")
+var Family = "The Waymans", Dad = "Guy", Mom = "Julie", Daughter = "Josie", Son = "Harry"; //Defines Multiple varibles as family members and assigns name values 
+var Family = Family.fontcolor("blue");//Sets font color for var "Son" and displays blue in browser
+var Dad = Dad.fontcolor("red"); //Sets font color for var "Son" and displays red in browser
+var Mom = Mom.fontcolor("green");//Sets font color for var "Son" and displays green in browser
+var Daughter = Daughter.fontcolor("black");//Sets font color for var "Son" and displays black in browser
+var Son = Son.fontcolor("orange");//Sets font color for var "Son" and displays orange in browser
+document.write(9 * 9); // Displays result of multiped numbers in browsers
